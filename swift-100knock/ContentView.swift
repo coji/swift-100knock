fe39eb2726811fbcba00d0bb797c9502f6e3a267
coji <coji@techtalk.jp>
2024-07-09 16:25:02 +0900

    Initial Commit

---
+++ swift-100knock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-100knock
+//
+//  Created by coji on 2024/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-100knock/swift_100knockApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_100knockApp.swift
+//  swift-100knock
+//
+//  Created by coji on 2024/07/09.
+//
+
+import SwiftUI
+
+@main
+struct swift_100knockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
